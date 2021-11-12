3f71417df2d801887ccfb95778d8679a4d72eb26
Jason StStieber <jrstiebeber@gmail.com>
2021-11-11 17:18:05 -0800

    completed both part A and B

--- day7/input.go
+++ day7/input.go
@@ -1,5 +1,13 @@
 package main
 
+const testInput = `Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.`
+
 const input = `Step E must be finished before step H can begin.
 Step Y must be finished before step T can begin.
 Step F must be finished before step S can begin.
