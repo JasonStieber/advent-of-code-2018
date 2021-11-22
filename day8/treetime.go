e9602d475c5577709d943f4ce475b9fc8cc4b118
Jason StStieber <jrstiebeber@gmail.com>
2021-11-22 15:43:23 -0800

    Answered part B

--- day8/treetime.go
+++ day8/treetime.go
@@ -10,7 +10,23 @@ type node struct {
 func main() {
 	head, _ := buildTree(0, input)
 	total := countData(head)
+	kidVals := countChildrenVal(head)
 	log.Printf("The answer to part A the sum of all the metadata is: %v \n", total)
+	log.Printf("The answer to part B the sum of the childrens data is: %v \n", kidVals)
+}
+
+func countChildrenVal(tree node) int {
+	total := 0
+	if len(tree.children) == 0 {
+		return countData(tree)
+	}
+	for i := 0; i < len(tree.metaData); i++ {
+		slot := tree.metaData[i]
+		if len(tree.children) >= slot {
+			total += countChildrenVal(*tree.children[slot-1])
+		}
+	}
+	return total
 }
 
 func countData(tree node) int {
