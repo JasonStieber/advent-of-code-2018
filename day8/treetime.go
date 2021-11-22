0e32c359ed78bb9ea27e28d87b9c05826c5a345c
Jason StStieber <jrstiebeber@gmail.com>
2021-11-22 15:31:04 -0800

    Completed part A

--- day8/treetime.go
+++ day8/treetime.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import "log"
 
+type node struct {
+	metaData []int
+	children []*node
+}
+
+func main() {
+	head, _ := buildTree(0, input)
+	total := countData(head)
+	log.Printf("The answer to part A the sum of all the metadata is: %v \n", total)
+}
+
+func countData(tree node) int {
+	total := 0
+	for i := 0; i < len(tree.children); i++ {
+		total += countData(*tree.children[i])
+	}
+	for j := 0; j < len(tree.metaData); j++ {
+		total += tree.metaData[j]
+	}
+	return total
+}
+
+func buildTree(loc int, data []int) (node, int) {
+	var n node
+	nLoc := loc + 2
+	numKids := data[loc]
+	numData := data[loc+1]
+	for i := 0; i < numKids; i++ {
+		var child node
+		child, nLoc = buildTree(nLoc, data)
+		n.children = append(n.children, &child)
+	}
+	for j := 0; j < numData; j++ {
+		n.metaData = append(n.metaData, data[nLoc+j])
+	}
+	return n, nLoc + numData
 }
